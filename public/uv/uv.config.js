6f54ee69e08bc5c0b4e0236159859a5549511f74
thattophatm0nkey82-spec <thattophatm0nkey82@gmail.com>
2026-03-29 13:15:58 -0600

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
-
 self.__uv$config = {
   prefix: "/uv/service/",
+  bare: "https://bare.marveloss.com/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
